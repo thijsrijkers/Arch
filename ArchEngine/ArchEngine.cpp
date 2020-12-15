3afba73561e23302b0c70b17218011c2af898331
thijsrijkers <thijsrijkers@live.nl>
2020-12-15 13:03:56 +0100

    🎉 First push of the basis of Arch

---
+++ ArchEngine/ArchEngine.cpp
@@ -0,0 +1,231 @@
+#include "consoleEngine.h"
+#include <fstream>
+#include <strstream>
+#include <algorithm>
+using namespace std;
+
+
+struct Vector3D
+{
+	float x, y, z;
+};
+
+struct Triangle
+{
+	Vector3D p[3];
+
+	wchar_t sym;
+	short col;
+};
+
+struct Mesh
+{
+	vector<Triangle> tris;
+
+	bool LoadFromObjectFile(string sFilename)
+	{
+		ifstream f(sFilename);
+		if (!f.is_open())
+			return false;
+
+		// Local cache of verts
+		vector<Vector3D> verts;
+
+		while (!f.eof())
+		{
+			char line[128];
+			f.getline(line, 128);
+
+			strstream s;
+			s << line;
+
+			char junk;
+
+			if (line[0] == 'v')
+			{
+				Vector3D v;
+				s >> junk >> v.x >> v.y >> v.z;
+				verts.push_back(v);
+			}
+
+			if (line[0] == 'f')
+			{
+				int f[3];
+				s >> junk >> f[0] >> f[1] >> f[2];
+				tris.push_back({ verts[f[0] - 1], verts[f[1] - 1], verts[f[2] - 1] });
+			}
+		}
+
+		return true;
+	}
+
+};
+
+struct mat4x4
+{
+	float m[4][4] = { 0 };
+};
+
+
+class ArchEngine : public Engine
+{
+public:
+	ArchEngine()
+	{
+		m_sAppName = L"Arch Engine";
+	}
+
+private:
+	Mesh cube;
+	mat4x4 matProj;
+
+	float fTheta;
+
+	void MultiplyMatrixVector(Vector3D& i, Vector3D& o, mat4x4& m)
+	{
+		o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+		o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+		o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+		float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+		if (w != 0.0f)
+		{
+			o.x /= w; o.y /= w; o.z /= w;
+		}
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		cube.tris = {
+
+			// SOUTH
+			{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+			// EAST                                                      
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+
+			// NORTH                                                     
+			{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+
+			// WEST                                                      
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+
+			// TOP                                                       
+			{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+			{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+
+			// BOTTOM                                                    
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+		};
+
+		// Projection Matrix
+		float fNear = 0.1f;
+		float fFar = 1000.0f;
+		float fFov = 90.0f;
+		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+
+		matProj.m[0][0] = fAspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar / (fFar - fNear);
+		matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+		matProj.m[2][3] = 1.0f;
+		matProj.m[3][3] = 0.0f;
+
+		// Clear Screen
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+		// Set up rotation matrices
+		mat4x4 matRotZ, matRotX;
+
+		//fTheta += 1.0f * fElapsedTime;
+
+		// Rotation Z
+		matRotZ.m[0][0] = cosf(fTheta);
+		matRotZ.m[0][1] = sinf(fTheta);
+		matRotZ.m[1][0] = -sinf(fTheta);
+		matRotZ.m[1][1] = cosf(fTheta);
+		matRotZ.m[2][2] = 1;
+		matRotZ.m[3][3] = 1;
+
+		// Rotation X
+		matRotX.m[0][0] = 1;
+		matRotX.m[1][1] = cosf(fTheta * 0.5f);
+		matRotX.m[1][2] = sinf(fTheta * 0.5f);
+		matRotX.m[2][1] = -sinf(fTheta * 0.5f);
+		matRotX.m[2][2] = cosf(fTheta * 0.5f);
+		matRotX.m[3][3] = 1;
+
+		// Draw Triangles
+		for (auto tri : cube.tris)
+		{
+			Triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
+
+			// Rotate in Z-Axis
+			MultiplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
+			MultiplyMatrixVector(tri.p[1], triRotatedZ.p[1], matRotZ);
+			MultiplyMatrixVector(tri.p[2], triRotatedZ.p[2], matRotZ);
+
+			// Rotate in X-Axis
+			MultiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
+
+			// Offset into the screen
+			triTranslated = triRotatedZX;
+			triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
+			triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
+			triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
+
+			// Project triangles from 3D --> 2D
+			MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+			MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+			MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+
+			// Scale into view
+			triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
+			triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
+			triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
+			triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
+			triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
+			triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
+
+			// Rasterize triangle
+			DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+				triProjected.p[1].x, triProjected.p[1].y,
+				triProjected.p[2].x, triProjected.p[2].y,
+				PIXEL_SOLID, FG_WHITE);
+
+		}
+
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		//fTheta += 1.0f * fElapsedTime;
+		return true;
+	}
+
+};
+
+
+
+
+int main()
+{
+	ArchEngine engine;
+	if (engine.ConstructConsole(256, 240, 4, 4))
+		engine.Start();
+	return 0;
+}
